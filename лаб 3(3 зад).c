6e136b4248482014618fbe96c264d262fb6bd3b7
waple7 <91288621+waple7@users.noreply.github.com>
2021-10-08 09:17:05 +0300

    Update лаб 3(3 зад).c

--- лаб 3(3 зад).c
+++ лаб 3(3 зад).c
@@ -3,7 +3,7 @@
 int main(){
     int number;
     scanf("%d",&number);
-    printf("%x\n",number);
+    printf("16 syst : %x\n",number);
     printf("%o >> 3 == %o\n", number, number >> 3);
     return 0;
 }
